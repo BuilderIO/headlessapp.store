b9e27271b1cf8de6f170d67711479aad66cdd900
Steve Sewell <steve@builder.io>
2020-12-03 19:27:48 -0800

    css

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ const GridItem = () => {
   return (
     <Link href="/components/101">
       <a className="p-8 bg-gray-600 rounded relative shadow group overflow-hidden md:py-16 text-center transition duration-150 ease-in-out">
-        <div>
+        <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-purple-600 opacity-0 group-hover:opacity-50 w-full h-full transform transition duration-100 ease-in-out z-10"></div>
+        <div className="relative z-10">
           <div className="mb-4">
             {logo ? (
               <img
@@ -26,7 +27,6 @@ const GridItem = () => {
             </p>
           </div>
         </div>
-        <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-purple-700 opacity-0 group-hover:opacity-50 w-full h-full transform transition duration-100 ease-in-out z-10"></div>
       </a>
     </Link>
   );
@@ -37,7 +37,7 @@ const IndexPage = () => {
     <Layout title="Home | Next.js + TypeScript Example">
       <div className="p-6 py-12 md:py-32 lg:py-36 bg-white shadow-sm border-b border-gray-200">
         <div className="max-w-2xl mx-auto text-center">
-          <h2 className="text-2xl sm:text-5xl md:text-6xl tracking-tight font-extrabold leading-tight text-transparent bg-gradient-to-r from-teal-500 via-blue-600 to-purple-800 bg-clip-text">
+          <h2 className="text-2xl sm:text-5xl md:text-6xl tracking-tight font-extrabold leading-tight text-transparent bg-gradient-to-r from-teal-500 via-blue-600 to-purple-600 bg-clip-text">
             Headless App Store
           </h2>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 opacity-75 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
