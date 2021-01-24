44317c345e488395811251ef66dbf8d8d7081ccc
Steve Sewell <steve@builder.io>
2021-01-24 13:01:42 -0800

    new teaser page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image";
+
 export const Index = () => (
-  <div className="h-screen flex items-center justify-center">
-    Coming soon :)
+  <div
+    style={{ background: "#0E201A", marginLeft: "calc(50% - 50vw)" }}
+    className="h-screen w-screen flex items-center justify-center pb-10"
+  >
+    <div className="m-auto w-11/12 max-w-4xl flex-col flex">
+      <Image
+        src="/assets/teaser-logo.png"
+        width="1436"
+        height="428"
+        alt="logo"
+      />
+
+      <a
+        rel="noreferrer"
+        href="mailto:goheadless@builder.io?subject=Please%20add%20me%20to%20the%20waitlist%20for%20HeadlessApp.Store&body=Thanks!"
+        className="text-center text-offwhite block mx-auto px-6 py-4 border-offwhite border-2 rounded mt-8"
+      >
+        Request early access
+      </a>
+    </div>
   </div>
 );
 
