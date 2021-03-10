915337142b6d51ae3b893763f4f410aacbd1a2ca
Steve Sewell <steve@builder.io>
2021-03-09 18:45:23 -0800

    nicer spinner

--- packages/site/components/GetApp.tsx
+++ packages/site/components/GetApp.tsx
@@ -594,7 +594,26 @@ export function GetApp(props: {
           className="bg-white p-12 shadow-2xl z-10 fixed bottom-0 left-0 right-0 top-1/6"
         >
           <div className="absolute top-0 left-0 items-center justify-center flex h-full w-full opacity-60">
-            Loading Builder.io visual editor...
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-black"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
           </div>
           <Show when={loadBuilder}>
             <div className="absolute top-0 right-0 left-0 bg-offwhite text-center border-b border-gray-80">
