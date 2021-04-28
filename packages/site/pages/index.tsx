39b8a3049f5325477d539559424a9fac02ee8714
Dylan Kendal <dylankendal@gmail.com>
2021-04-28 16:32:25 -0400

    refactor: Make homepage user SSR

--- packages/site/pages/index.tsx
+++ packages/site/pages/index.tsx
@@ -1,17 +1,28 @@
-import { BuilderComponent } from '@builder.io/react';
+import builder, { BuilderComponent } from '@builder.io/react';
+import { InferGetStaticPropsType } from 'next';
 import Layout from '../components/Layout';
 
-export const Index = () => {
+type Props = Exclude<InferGetStaticPropsType<typeof getStaticProps>, { errors: any }>;
+
+export const Index = ({ content }: Props) => {
   return (
     <Layout hideHeaderAndFooter>
       <div
         style={{ marginLeft: 'calc(50% - 50vw)' }}
         className="flex items-center justify-center w-screen h-screen pb-10 bg-dark"
       >
-        <BuilderComponent model="page" />
+        <BuilderComponent model="page" content={content} />
       </div>
     </Layout>
   );
+};
+
+export const getStaticProps = async () => {
+  return builder
+    .get('page', { url: '/' })
+    .promise()
+    .then((content: any) => ({ props: { content, revalidate: true, notFound: !content } }))
+    .catch((err: { message: any }) => ({ props: { errors: err.message } }));
 };
 
 export default Index;
